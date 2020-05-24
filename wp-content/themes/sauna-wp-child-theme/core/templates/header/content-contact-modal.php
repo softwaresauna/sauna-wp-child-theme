3d76b28df121f357d5829bda374d848fd67b421e
Dominik Mandalinic <dominik.mandalinic@gmail.com>
2020-05-25 00:08:37 +0200

    Modal contact form

--- wp-content/themes/sauna-wp-child-theme/core/templates/header/content-contact-modal.php
+++ wp-content/themes/sauna-wp-child-theme/core/templates/header/content-contact-modal.php
@@ -4,13 +4,15 @@
   <div class="modal fade popup-modal" id="popup-modal" role="dialog">
     <div class="modal-content contact">
         <div class="row">
-          <div class="col-sm-12 modal-content-contact">
-<!--           <?php if (isset($redux_ThemeTek['tek-modal-title']) && $redux_ThemeTek['tek-modal-title'] != '' ) : ?>
-              <h2><?php echo esc_attr($redux_ThemeTek['tek-modal-title']); ?></h2>
-          <?php endif; ?> -->
-          <?php if (isset($redux_ThemeTek['tek-modal-subtitle']) && $redux_ThemeTek['tek-modal-subtitle'] != '' ) : ?>
-              <p><?php echo wp_kses_post($redux_ThemeTek['tek-modal-subtitle']); ?></p>
-          <?php endif; ?>
+          <div class="col-sm-8 modal-content-contact">
+
+<!--              <h2>--><?php //echo __("Let's do some cool stuff together", "sauna-modal-contact") ?><!--</h2>-->
+<!--          --><?php //if (isset($redux_ThemeTek['tek-modal-subtitle']) && $redux_ThemeTek['tek-modal-subtitle'] != '' ) : ?>
+<!--              <p>--><?php //echo wp_kses_post($redux_ThemeTek['tek-modal-subtitle']); ?><!--</p>-->
+<!--          --><?php //endif; ?>
+              <p>
+                  <?php echo __("Have a project? Let's chat.", "sauna-modal-contact")?>
+              </p>
           <?php if (isset($redux_ThemeTek['tek-business-phone']) && $redux_ThemeTek['tek-business-phone'] != '' ) : ?>
               <div class="key-icon-box icon-default icon-left cont-left">
                   <i class="iconsmind-Phone fa"></i>
@@ -24,6 +26,9 @@
               </div>
           <?php endif; ?>
           </div>
+            <div class="col-sm-4 modal-content-contact ilkka">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ilkka.jpg"/>
+            </div>
         </div>
       <button type="button" class="close" data-dismiss="modal">&times;</button>
   </div>
